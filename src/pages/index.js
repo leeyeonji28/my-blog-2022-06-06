876b79619f97de22023e97bcbac8622c771af3cb
leeyeonji28 <yeon102800@gmail.com>
2022-06-06 22:16:14 +0900

    gnb 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
+import { Link } from "gatsby";
 import * as React from "react";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
+      <div style={{ display: "flex", gap: 20 }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/test">Test</Link>
+        <Link to="/test-space">Test-Space</Link>
+      </div>
+
       <h1>Hone Page</h1>
     </main>
   );
