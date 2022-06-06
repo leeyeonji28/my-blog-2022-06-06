9df23073ce98abf7bdd002c0208b02eec8a584ba
leeyeonji28 <yeon102800@gmail.com>
2022-06-06 22:32:00 +0900

    container에 css설정

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,23 @@
 import { Link } from "gatsby";
 import React, { Children } from "react";
 
+import { container } from "./layout.module.css";
+
 function Layout({ children }) {
   return (
     <>
-      <main>
-        <div style={{ display: "flex", gap: 20 }}>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/test">Test</Link>
-          <Link to="/test-space">Test-Space</Link>
-        </div>
-        {children}
-        <div>Copyright...</div>
-      </main>
+      <div className={container}>
+        <main>
+          <div style={{ display: "flex", gap: 20 }}>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/test">Test</Link>
+            <Link to="/test-space">Test-Space</Link>
+          </div>
+          {children}
+          <div>Copyright...</div>
+        </main>
+      </div>
     </>
   );
 }
